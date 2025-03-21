b0ce1beb30c024f59d2d8f28962717861f491819
Aryan Patel <aryanpatel@Aryans-MacBook-Air.local>
2025-03-22 00:24:03 +0530

    newwwwww

--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -2,7 +2,7 @@ import { create } from "zustand";
 
 export const useThemeStore = create((set) => ({
 
-    theme: localStorage.getItem("theme") || "Valentine",
+    theme: localStorage.getItem("theme") || "valentine",
 
     setTheme: (theme) => {
         localStorage.setItem("theme", theme);
